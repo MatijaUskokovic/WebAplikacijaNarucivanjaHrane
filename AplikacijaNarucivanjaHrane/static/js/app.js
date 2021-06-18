45444bdddfb4343fb6f43c7f1306ed6b3a48771f
Matija Uskokovic <uskokovic.ra76.2018@uns.ac.rs>
2021-06-18 19:08:32 +0200

    formiran glavni toolbar i napravljeno navigiranje na pocetnu stranicu i stranicu za logovanje ili registraciju

--- AplikacijaNarucivanjaHrane/static/js/app.js
+++ AplikacijaNarucivanjaHrane/static/js/app.js
@@ -1,15 +1,15 @@
-const Product = { template: '<edit-product></edit-product>' }
-const Products = { template: '<products></products>' }
+const Restaurants = { template: '<restaurants></restaurants>'}
+const LoginAndRegistration = { template: '<loginAndRegistration></loginAndRegistration>'}
 
 const router = new VueRouter({
 	mode: 'hash',
 	  routes: [
-		{ path: '/', name: 'home', component: Products},
-	    { path: '/products/:id', component: Product}
+		{ path: '/', name: 'Početna', component: Restaurants},
+		{ path: '/login', name:'login', component:LoginAndRegistration}
 	  ]
 });
 
 var app = new Vue({
 	router,
-	el: '#products'
+	el: '#mainView'
 });
